596d9f7504d0c327710dc2f055ec892d9a13425d
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 19:27:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   text_out = []
   if (array.length > 0)
     text_out << "The line is currently: "
-    for i in 0..array.length do
+    for i in 0..(array.length-1) do
       text_out << "#{i+1}. #{array[i]} "
     end
   else
