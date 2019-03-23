3e3360a2f02200fb8d0adc7dbe016b627e9068ac
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 19:14:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(array)
   if (array.length = 0)
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
+    puts "The line is currently:"
 
 def take_a_number(array,name)
   array << name
